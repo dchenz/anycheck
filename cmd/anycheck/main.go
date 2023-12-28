9d78429de0659cd0d524b08432ac58942728588f
Danny <51312422+dchenz@users.noreply.github.com>
2023-12-28 15:00:44 +0000

    Added flags to set preference

--- cmd/anycheck/main.go
+++ cmd/anycheck/main.go
@@ -1,16 +1,26 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	anycheck "github.com/dchenz/anycheck/analyzer"
 	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
+var settings anycheck.LinterSettings
+
 func main() {
-	a, err := anycheck.NewAnalyzer(anycheck.LinterSettings{})
+	flag.Parse()
+
+	a, err := anycheck.NewAnalyzer(settings)
 	if err != nil {
 		log.Fatalf("anycheck error: %s", err.Error())
 	}
 	singlechecker.Main(a)
+}
+
+func init() {
+	flag.StringVar(&settings.Prefer, "prefer", "any",
+		"linter preference: either 'any' or 'interface'")
 }
